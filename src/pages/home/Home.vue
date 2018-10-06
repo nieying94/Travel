8043b5db3ee955caa6270bf55475a26bfcb8efbf
nieying94 <nieying94@hotmail.com>
2018-10-06 15:02:44 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,16 @@
 <!-- template定义了组件的模板 -->
 <template>
-    <div> world</div>
+    <home-header> </home-header>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 // 导出一个对象，里面定义一个组件的名字
 export default{
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
